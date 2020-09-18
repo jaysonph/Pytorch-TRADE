919859a8e8ea193654e249e5cd67613df10b3606
jaysonph <iamjaysonph@gmail.com>
2020-09-18 18:38:41 +0800

    Vocabulary class for tokenization and create word2id

--- tools/vocab.py
+++ tools/vocab.py
@@ -3,25 +3,25 @@ import json
 import numpy as np
 import torch
 from tqdm import tqdm
-from embeddings import GloveEmbedding, KazumaCharEmbedding, ConcatEmbedding
+from embeddings import GloveEmbedding
 
 class Vocab():
-	def __init__(self, pretrained_embed_path=None, corpus=None):
+	def __init__(self, pretrained_word_embed_path=None, corpus=None):
 		'''
-		if pretrained_embed_path is provided, corpus will be ignored.
+		if pretrained_word_embed_path is provided, corpus will be ignored.
 
-		Input:
+		Args:
 		- domain_dict_path (str): path to domain dict json file
 		- pretrained_embed (str): path to pretrained embedding json file
 		- corpus (list): list of long text(str)
 		'''
 
-		assert not (pretrained_embed_path == None and corpus == None), 'Either <pretrained_embed_path> or <corpus> must be provided'
+		assert not (pretrained_word_embed_path == None and corpus == None), 'Either <pretrained_word_embed_path> or <corpus> must be provided'
 
-		if pretrained_embed_path == None:
-			self.word2id, self.embed_list = self._create_word2id_from_corpus(corpus)
+		if pretrained_word_embed_path == None:
+			self.word2id, self.word_embed_list = self.create_word2id_from_corpus(corpus)
 		else:
-			self.word2id, self.embed_list = self._load_from_pretrained_embed(pretrained_embed_path)
+			self.word2id, self.word_embed_list = self.load_from_pretrained_embed(pretrained_word_embed_path)
 
 
 		self.id2word = {idx: word for word, idx in self.word2id.items()}
@@ -30,43 +30,42 @@ class Vocab():
 		'''
 		Tokenize text into words.
 
-		Input:
+		Args:
 		- text (str)
 
-		Output:
+		Returns:
 		- word_seq (list): list of words(str)
 		'''
 		word_seq = [w.strip() for w in text.split(' ')]
 		return word_seq
 
-	def words_to_index(self, word_seq):
+	def word_to_index(self, word):
 		'''
 		Convert word to word index. If word is unseen, it will be treated as unknown token ('UNK') 
 
-		Input:
+		Args:
 		- word_seq (list): list of words(str)
 
-		Output:
+		Returns:
 		- word_id_seq (list): list of word indices
 		'''
-		word_id_seq = [self.word2id.get(word, self.word2id['[UNK]']) for word in word_seq]
-		return word_id_seq
+		word_id = self.word2id.get(word, self.word2id['[UNK]'])
+		return word_id
 
-	def _create_word2id_from_corpus(self, corpus):
+	def create_word2id_from_corpus(self, corpus):
 		'''
 		tokenize the corpus into words and create word2id from them and save the embeddings to json file
 
-		Input:
+		Args:
 		- corpus (list): list of long text(str)
 
-		Output:
+		Returns:
 		- word2id (dict)
-		- embed_list (list): list of vectors(list)
+		- word_embed_list (list): list of word vectors(list)
 		'''
 
-		embed = [GloveEmbedding('common_crawl_840', d_emb=300, show_progress=True), KazumaCharEmbedding()]
-		embed_dim = sum([e.d_emb for e in embed])
-		embed = ConcatEmbedding(embed, default='zero')
+		embed = GloveEmbedding('common_crawl_840', d_emb=300, show_progress=True)
+		embed_dim = embed.d_emb
 
 		word2id = {
 			'[UNK]': 0,
@@ -77,36 +76,39 @@ class Vocab():
 		}
 
 		# Randomly initialize vectors for special tokens
-		embed_list = [np.random.uniform(-1,1,embed_dim).tolist() for _ in range(len(word2id))]
+		word_embed_list = [np.random.uniform(-1,1,embed_dim).tolist() for _ in range(len(word2id))]
 
 		for passage in tqdm(corpus, desc='Creating vocabulary from data corpus: '):
 			word_seq = self.tokenize(passage)
 			for word in word_seq:
 				if word2id.get(word) == None:
 					word2id[word] = len(word2id)
-					embed_list.append(embed.emb(word))
+					word_vec = embed.emb(word) if embed.lookup(word) != None else np.random.uniform(-1,1,embed_dim).tolist()
+					word_embed_list.append(word_vec)
 
 		# Save the embeddings json file
 		save_path = './trained_vocab/'
 		if not os.path.exists(save_path):
 			os.makedirs(save_path)
 
-		with open(os.path.join(save_path, 'embeddings.json'), 'w') as f:
-			embeddings_dict = {word: embed_list[idx] for word, idx in word2id.items()}
+		with open(os.path.join(save_path, 'word_embeddings.json'), 'w') as f:
+			embeddings_dict = {word: word_embed_list[idx] for word, idx in word2id.items()}
 			json.dump(embeddings_dict, f)
 
-		return word2id, embed_list
+		return word2id, word_embed_list
 
-	def _load_from_pretrained_embed(self, pretrained_embed_path):
+	def load_from_pretrained_embed(self, pretrained_word_embed_path):
 		'''
-		Input:
+		Args:
 		- pretrained_embed_path (str): path to pretrained embedding json file
 		'''
-		with open(pretrained_embed_path, 'r') as f:
+		with open(pretrained_word_embed_path, 'r') as f:
 			embeddings_dict = json.load(f)  # dict
 
+		word_embed_list = []
+		word2id = {}
 		for idx, (word, vector) in enumerate(tqdm(embeddings_dict.items(), desc='Reading pretrained embeddings: ')):
 			word2id[word] = idx
-			embed_list.append(vector)
+			word_embed_list.append(vector)
 
-		return word2id, embed_list
+		return word2id, word_embed_list
